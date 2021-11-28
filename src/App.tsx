59f1d021bd97f9ccc2ab59bf9e64b6300d03f2c1
김형우 <horrorkist@Horrorkist.local>
2021-11-28 14:51:13 +0900

    trello-clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import {
+  DragDropContext,
+  Droppable,
+  DropResult,
+  Draggable,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import { IToDoState, toDoState } from "./atoms";
+import Board from "./Components/Board";
+import TrashBin from "./Components/TrashCan";
+import CreateNewBoard from "./Components/CreateNewBoard";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 1000px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: space-evenly;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 300px);
+  gap: 20px;
+`;
 
 function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({
+    type,
+    draggableId,
+    destination,
+    source,
+  }: DropResult) => {
+    if (!destination) return;
+    if (destination?.droppableId === "trashBin") {
+      // delete toDo
+      setToDos((oldToDos) => {
+        const sourceCopy = [...oldToDos[source.droppableId]];
+        return {
+          ...oldToDos,
+          [source.droppableId]: [
+            ...oldToDos[source.droppableId].slice(0, source.index),
+            ...oldToDos[source.droppableId].slice(source.index + 1),
+          ],
+        };
+      });
+      return;
+    }
+
+    if (type === "board") {
+      if (destination.index === source.index) return;
+      setToDos((oldToDos) => {
+        const keyList = Object.keys(toDos);
+        let sourceKey: string, destKey: string;
+        keyList.forEach((element, index) => {
+          if (index === source.index) sourceKey = element;
+          if (index === destination.index) destKey = element;
+        });
+        const newToDos: IToDoState = {};
+        keyList.forEach((element) => {
+          if (element === sourceKey) return;
+          if (element === destKey && source.index > destination.index) {
+            newToDos[sourceKey] = [...oldToDos[sourceKey]];
+          }
+          newToDos[element] = [...oldToDos[element]];
+          if (element === destKey && source.index < destination.index) {
+            newToDos[sourceKey] = [...oldToDos[sourceKey]];
+          }
+        });
+        return newToDos;
+      });
+      return;
+    }
+
+    if (destination?.droppableId === source.droppableId) {
+      setToDos((oldToDos) => {
+        const boardCopy = [...oldToDos[source.droppableId]];
+        boardCopy.splice(
+          destination?.index,
+          0,
+          ...boardCopy.splice(source.index, 1)
+        );
+        return {
+          ...oldToDos,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+
+    if (destination?.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((oldToDos) => {
+        const sourceCopy = [...oldToDos[source.droppableId]];
+        const destCopy = [...oldToDos[destination.droppableId]];
+        const targetElement = sourceCopy.splice(source.index, 1);
+        destCopy.splice(destination.index, 0, ...targetElement);
+        return {
+          ...oldToDos,
+          [source.droppableId]: sourceCopy,
+          [destination.droppableId]: destCopy,
+        };
+      });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <CreateNewBoard />
+        <Droppable
+          droppableId="droppableBoards"
+          type="board"
+          direction="horizontal"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {(provided, snapshot) => (
+            <Boards ref={provided.innerRef}>
+              {Object.keys(toDos).map((boardId, index) => (
+                <Board
+                  boardIndex={index}
+                  boardId={boardId}
+                  key={boardId}
+                  toDos={toDos[boardId]}
+                />
+              ))}
+              {provided.placeholder}
+            </Boards>
+          )}
+        </Droppable>
+        <TrashBin />
+      </Wrapper>
+    </DragDropContext>
   );
 }
 
